77b09204fc3d7cb66a33bbfeab800ac74bf5d424
Luciano Muñoz <luchisds@gmail.com>
2017-12-05 02:47:54 -0300

    checking login route

---
+++ src/models/apikey.php
@@ -0,0 +1,10 @@
+<?php
+
+use \Illuminate\Database\Eloquent\Model as Model;
+
+class ApiKey extends Model {
+
+  protected $table = 'api-keys';
+  public $timestamps = false;
+
+}
--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,7 @@ use Slim\Http\Response;
 // Import DB Models
 require __DIR__ . '/../src/models/product.php';
 require __DIR__ . '/../src/models/purchase.php';
+require __DIR__ . '/../src/models/apikey.php';
 // Import Validators
 require __DIR__ . '/../src/validators.php';
 
@@ -112,7 +113,42 @@ $app->get('/v1/compras', function(Request $request, Response $response, array $a
 
 // Login to the API
 $app->post('/login', function(Request $request, Response $response, array $args) {
-  $data = array('message' => 'Login');
+  $headerAuthentication = $request->getHeader('authentication');
+  $headerTimestamp = $request->getHeader('timestamp');
 
-  return $response->withJson($data);
+  //Check timestamp validation
+
+  if(empty($headerAuthentication) || empty($headerTimestamp)) {
+    $messages = array('error' => 'Cabeceras no encontradas');
+  } else {
+    if(strpos(strtoupper($headerAuthentication[0]), 'HMAC ') === false) {
+      $messages = array('error' => 'Cabecera de autenticación no hallada');
+    } else {
+      $authKeySig = substr($headerAuthentication, 5);
+      if(count(explode(':', $authKeySig)) !== 2) {
+        $messages = array('error' => 'La cabecera de autenticacion no es válida');
+      } else {
+        list($publicKey, $hmacSignature) = explode(':', $authKeySig);
+
+        $secretKey = ApiKey::where('public_id', $publicKey)->value('secret_key')->first();
+        if($secretKey === null) {
+          $messages = array('error' => 'Clave pública no válida');
+        } else {
+          $payload = $request->getMethod() . '\n';
+          $payload .= $request->getUri() . '\n';
+          $payload .= $headerTimestamp . '\n';
+
+          $hash = hash_hmac('sha256', $payload, $secretKey, false);
+
+          if($hmacSignature === $hash) {
+            $messages = array('status' => 'TOKEN');
+          } else {
+            $messages = array('status' => 'Signature no válido');
+          }
+        }
+      }
+    }
+  }
+
+  return $response->withJson($messages);
 });
