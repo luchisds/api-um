f25836058e01e5d56ea1df6e91c477cb669c8895
Luciano Muñoz <luchisds@gmail.com>
2017-12-05 18:02:25 -0300

    add JWT middleware authorization

--- src/middleware.php
+++ src/middleware.php
@@ -1,4 +1,38 @@
 <?php
 // Application middleware
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+//use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Parser;
+use Lcobucci\JWT\ValidationData;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+
 // e.g: $app->add(new \Slim\Csrf\Guard);
+class JWTMiddleware {
+
+  public function __invoke(Request $request, Response $response, callable $next) {
+    $tokenAuthentication = $request->getHeader('token-auth');
+
+    if(empty($tokenAuthentication) || strlen($tokenAuthentication[0]) === 0) {
+      //return $response->withStatus(401);
+      return $response->withJson(array('error' => 'Cabecera de token no válida'));
+    } else {
+      $token = (new Parser())->parse((string) $tokenAuthentication[0]); // Parses from a string
+      $signer = new Sha256();
+      if($token->verify($signer, 'testing') !== true) {
+        return $response->withJson(array('error' => 'Signature no válido'));
+      } else {
+        $dataTokenValidation = new ValidationData();
+        $dataTokenValidation->setCurrentTime(time());
+        if($token->validate($dataTokenValidation) !== true) {
+          return $response->withJson(array('error' => 'Token vencido. Vuelva a loguearse.'));
+        } else {
+          return $next($request, $response);
+        }
+      }
+    }
+  }
+
+}
--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,10 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+
+
 // Import DB Models
 require __DIR__ . '/../src/models/product.php';
 require __DIR__ . '/../src/models/purchase.php';
@@ -12,104 +16,104 @@ require __DIR__ . '/../src/validators.php';
 
 
 // Routes
+$app->group('/v1', function () use($app) {
+  // Update price and stock by product color
+  $app->post('/productos/{id}', function(Request $request, Response $response, array $args) {
+    $data = $request->getParsedBody();
 
-// Update price and stock by product color
-$app->post('/v1/productos/{id}', function(Request $request, Response $response, array $args) {
-  $data = $request->getParsedBody();
-
-  //validation
-  $toClean = new \stdClass();
-  $toClean->product_id = $args['id'];
-  $toClean->price = $data['price'];
-  $toClean->stock = $data['stock'];
-  $validation = validateProductUpdate($toClean);
-
-  //actions
-  if($validation['isValid'] === true) {
-    $product = Product::where('product_id', $toClean->product_id)->first();
-    if($product === null) {
-      $messages = array('error' => 'Código de producto inexistente');
-    } else {
-      $product->price = $toClean->price;
-      $product->stock = $toClean->stock;
-      $product->save();
-      $messages = array('status' => 'Actualización correcta');
-    }
-  } else {
-    $messages = array('validation' => $validation['errors']);
-  }
-
-  return $response->withJson($messages);
-});
-
-
-// Update purchases by product
-$app->post('/v1/compras/{id}', function(Request $request, Response $response, array $args) {
-  $data = $request->getParsedBody();
-
-  //validation
-  $toClean = new \stdClass();
-  $toClean->product_id = $args['id'];
-  $toClean->quantity = $data['quantity'];
-  $toClean->date = $data['date'];
-  $toClean->invoice = $data['invoice'];
-  $validation = validatePurchaseUpdate($toClean);
-
-  //actions
-  if($validation['isValid'] === true) {
-    if(Product::where('product_id', $toClean->product_id)->first() === null) {
-      $messages = array('error' => 'Código de producto inexistente');
-    } elseif(Purchase::where('product_id', $toClean->product_id)->where('invoice', $toClean->invoice)->first() !== null) {
-      $messages = array('error' => 'Compra ya registrada anteriormente');
-    } else {
-      $purchase = new Purchase;
-      $purchase->product_id = $toClean->product_id;
-      $purchase->quantity = $toClean->quantity;
-      $purchase->date = $toClean->date;
-      $purchase->invoice = $toClean->invoice;
-      $purchase->save();
-      $messages = array('status' => 'Actualización correcta');
-    }
-  } else {
-    $messages = array('validation' => $validation['errors']);
-  }
-
-  return $response->withJson($messages);
-});
-
-
-// Check purchases made between two dates
-$app->get('/v1/compras', function(Request $request, Response $response, array $args) {
-  $data = $request->getQueryParams();
-
-  if(isset($data['fromDate']) && isset($data['toDate'])) {
     //validation
     $toClean = new \stdClass();
-    $toClean->fromDate = $data['fromDate'];
-    $toClean->toDate = $data['toDate'];
-    $validation = validatePurchaseCheck($toClean);
+    $toClean->product_id = $args['id'];
+    $toClean->price = $data['price'];
+    $toClean->stock = $data['stock'];
+    $validation = validateProductUpdate($toClean);
 
     //actions
     if($validation['isValid'] === true) {
-      $purchase = Purchase::whereBetween('date', [$toClean->fromDate, $toClean->toDate])->get();
-      $messages = array('status' => 'ok', 'data' => $purchase);
+      $product = Product::where('product_id', $toClean->product_id)->first();
+      if($product === null) {
+        $messages = array('error' => 'Código de producto inexistente');
+      } else {
+        $product->price = $toClean->price;
+        $product->stock = $toClean->stock;
+        $product->save();
+        $messages = array('status' => 'Actualización correcta');
+      }
     } else {
       $messages = array('validation' => $validation['errors']);
     }
-  } else {
-    if(isset($data['fromDate'])) {
-      $messages = array('error' => 'Falta fecha hasta (toDate)');
-    } elseif(isset($data['toDate'])) {
-      $messages = array('error' => 'Falta fecha desde (fromDate)');
+
+    return $response->withJson($messages);
+  });
+
+
+  // Update purchases by product
+  $app->post('/compras/{id}', function(Request $request, Response $response, array $args) {
+    $data = $request->getParsedBody();
+
+    //validation
+    $toClean = new \stdClass();
+    $toClean->product_id = $args['id'];
+    $toClean->quantity = $data['quantity'];
+    $toClean->date = $data['date'];
+    $toClean->invoice = $data['invoice'];
+    $validation = validatePurchaseUpdate($toClean);
+
+    //actions
+    if($validation['isValid'] === true) {
+      if(Product::where('product_id', $toClean->product_id)->first() === null) {
+        $messages = array('error' => 'Código de producto inexistente');
+      } elseif(Purchase::where('product_id', $toClean->product_id)->where('invoice', $toClean->invoice)->first() !== null) {
+        $messages = array('error' => 'Compra ya registrada anteriormente');
+      } else {
+        $purchase = new Purchase;
+        $purchase->product_id = $toClean->product_id;
+        $purchase->quantity = $toClean->quantity;
+        $purchase->date = $toClean->date;
+        $purchase->invoice = $toClean->invoice;
+        $purchase->save();
+        $messages = array('status' => 'Actualización correcta');
+      }
     } else {
-      $purchase = Purchase::all();
-      $messages = array('status' => 'ok', 'data' => $purchase);
+      $messages = array('validation' => $validation['errors']);
     }
-  }
 
-  return $response->withJson($messages);
-});
+    return $response->withJson($messages);
+  });
 
+
+  // Check purchases made between two dates
+  $app->get('/compras', function(Request $request, Response $response, array $args) {
+    $data = $request->getQueryParams();
+
+    if(isset($data['fromDate']) && isset($data['toDate'])) {
+      //validation
+      $toClean = new \stdClass();
+      $toClean->fromDate = $data['fromDate'];
+      $toClean->toDate = $data['toDate'];
+      $validation = validatePurchaseCheck($toClean);
+
+      //actions
+      if($validation['isValid'] === true) {
+        $purchase = Purchase::whereBetween('date', [$toClean->fromDate, $toClean->toDate])->get();
+        $messages = array('status' => 'ok', 'data' => $purchase);
+      } else {
+        $messages = array('validation' => $validation['errors']);
+      }
+    } else {
+      if(isset($data['fromDate'])) {
+        $messages = array('error' => 'Falta fecha hasta (toDate)');
+      } elseif(isset($data['toDate'])) {
+        $messages = array('error' => 'Falta fecha desde (fromDate)');
+      } else {
+        $purchase = Purchase::all();
+        $messages = array('status' => 'ok', 'data' => $purchase);
+      }
+    }
+
+    return $response->withJson($messages);
+  });
+})->add(new JWTMiddleware());
 
 // Login to the API
 $app->post('/login', function(Request $request, Response $response, array $args) {
@@ -124,24 +128,33 @@ $app->post('/login', function(Request $request, Response $response, array $args)
     if(strpos(strtoupper($headerAuthentication[0]), 'HMAC ') === false) {
       $messages = array('error' => 'Cabecera de autenticación no hallada');
     } else {
-      $authKeySig = substr($headerAuthentication, 5);
+      $authKeySig = substr($headerAuthentication[0], 5);
       if(count(explode(':', $authKeySig)) !== 2) {
         $messages = array('error' => 'La cabecera de autenticacion no es válida');
       } else {
         list($publicKey, $hmacSignature) = explode(':', $authKeySig);
 
-        $secretKey = ApiKey::where('public_id', $publicKey)->value('secret_key')->first();
-        if($secretKey === null) {
+        //$secretKey = ApiKey::where('public_key', $publicKey)->value('secret_key');
+        $apiKey = ApiKey::where('public_key', $publicKey)->first();
+        if($apiKey->secret_key === null) {
           $messages = array('error' => 'Clave pública no válida');
         } else {
-          $payload = $request->getMethod() . '\n';
-          $payload .= $request->getUri() . '\n';
-          $payload .= $headerTimestamp . '\n';
+          $uri = $request->getUri();
 
-          $hash = hash_hmac('sha256', $payload, $secretKey, false);
+          $payload = $request->getMethod() . '&';
+          $payload .= $uri->getPath() . '&';
+          $payload .= $headerTimestamp[0];
+
+          $hash = hash_hmac('sha256', $payload, $apiKey->secret_key, false);
 
           if($hmacSignature === $hash) {
-            $messages = array('status' => 'TOKEN');
+            $signer = new Sha256();
+            $token = (new Builder())->setIssuedAt(time()) // The time that the token was issue
+                                    ->setExpiration(time() + 300) // Set the expiration time of the token in 1 hs. (3600)
+                                    ->sign($signer, 'testing') // Signature using "testing" as key
+                                    ->getToken();
+
+            $messages = array('token' => ((string) $token));
           } else {
             $messages = array('status' => 'Signature no válido');
           }
